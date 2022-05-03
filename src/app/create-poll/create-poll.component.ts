eff964d439279525d9dccfbe1d4636acbd6c73a1
CotutRaul <92167690+CotutRaul@users.noreply.github.com>
2022-05-03 19:25:51 +0300

    Beta

---
+++ src/app/create-poll/create-poll.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+
+import { Poll } from '../poll';
+import { PollService } from '../poll-service.service';
+
+@Component({
+  selector: 'app-create-poll',
+  templateUrl: './create-poll.component.html',
+  styleUrls: ['./create-poll.component.css']
+})
+export class CreatePollComponent implements OnInit {
+  poll:Poll = new Poll();
+  date!: Date;
+  constructor(private servicePoll : PollService, private router : Router ) { }
+  ngOnInit(): void {
+
+  }
+  saveEmployee(){
+    this.servicePoll.createPoll(this.poll).subscribe( data =>{
+      console.log(data);
+      console.log(this.poll);
+      this.goToEmployeeList();
+    },
+    error => console.log(error));
+  }
+
+  goToEmployeeList(){
+    this.router.navigate(['/home']);
+  }
+  
+  onSubmit(){
+    console.log(this.poll);
+    this.saveEmployee();
+  }
+  cancel(){
+    this.router.navigate(['/home']);
+  }
+}
